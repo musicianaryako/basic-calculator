08d14a5fac1603833125309e93290f45c1c9a442
musicianaryako <43232358+musicianaryako@users.noreply.github.com>
2025-01-31 20:17:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 
+'use strict';
+
+let answer = 0;
+function calculator(num1, operator, num2) {
+  if (operator === '+') {
+    answer = num1 + num2;
+  } else if (operator === '-') {
+    answer = num1 - num2;
+  } else if (operator === '*') {
+    answer = num1 * num2;
+  } else if (operator === '/') {
+    answer = num1 / num2;
+  }
+  return answer;
+}
+
+console.log(calculator(9, '+', 3)); // 12
+console.log(calculator(9, '-', 3)); // 6
+console.log(calculator(9, '*', 3)); // 27
+console.log(calculator(9, '/', 3)); // 3
